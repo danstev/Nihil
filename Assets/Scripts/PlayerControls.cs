2e6bfd737330b018f85379e9f7f71707cfd0a52e
Dan Ward <dan.steve@hotmail.co.uk>
2018-03-09 18:30:10 +0000

    controls

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,13 +4,67 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Transform cam;
+    private float yRotation, xRotation, currentXRotation, currentYRotation, yRotationV, xRotationV;
+    public float lookSensitivity = 5;
+    public float lookSmoothnes = 0.1f;
+    public float bottom = 60F;
+    public float top = -60f;
+
+    //Movement
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+        cam = GetComponentInChildren<Camera>().transform;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        mouselook();
+        movement();
+    }
+
+    void movement()
+    {
+        //Movement
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void mouselook()
+    {
+        yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+        xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+        xRotation = Mathf.Clamp(xRotation, -80, 100);
+        currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationV, lookSmoothnes);
+        currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationV, lookSmoothnes);
+        if (currentXRotation > bottom)
+        {
+            currentXRotation = bottom;
+            xRotation = bottom;
+        }
+
+        if (currentXRotation < top)
+        {
+            currentXRotation = top;
+            xRotation = top;
+        }
+
+        transform.rotation = Quaternion.Euler(0, currentYRotation, 0);
+        cam.transform.rotation = Quaternion.Euler(currentXRotation, currentYRotation, 0);
+    }
 }
