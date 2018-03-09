40f6ef0d7c344f755c018ce13b0f94a0e70c49ba
Dan Ward <dan.steve@hotmail.co.uk>
2018-03-09 18:45:43 +0000

    redo stats

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public int health = 0;
+    public int maxHealth = 100;
+    private bool dead = false;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (dead)
+        {
+            return;
+        }
+    }
+
+    public void TakeDamage(int incoming)
+    {
+        health -= incoming;
+        Debug.Log("Damage: " + incoming + " has been taken by: " + gameObject.name + ".");
+    }
 }
