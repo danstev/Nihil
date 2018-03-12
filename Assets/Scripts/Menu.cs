c614cdc5d8e258b451d5d5c58e96e763407edd1f
Dan Ward <dan.steve@hotmail.co.uk>
2018-03-12 14:59:42 +0000

    menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public void NewGame()
+    {
+        Debug.Log("New Game pressed.");
+    }
+
+    public void JoinGame()
+    {
+        Debug.Log("Join Game pressed.");
+    }
+
+    public void Options()
+    {
+        Debug.Log("Options pressed.");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit Game pressed.");
+        Application.Quit();
+    }
+
+
+}
