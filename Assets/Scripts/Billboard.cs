51654626cd7f454c6f5f50e5bb942859551d09c3
Dan Ward <dan.steve@hotmail.co.uk>
2018-06-09 22:52:49 +0100

    old bilboarding scripts added, not used yet

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour
+{
+
+    void Update() 
+	{
+        transform.LookAt( new Vector3 (Camera.main.transform.position.x, 0.75f, Camera.main.transform.position.z), Vector3.up);
+    }
+}
---
+++ Assets/Scripts/BillboardSprite.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardSprite : MonoBehaviour {
+
+    private SpriteRenderer r;
+    public Sprite front;
+    public Sprite left;
+    public Sprite right;
+    public Sprite back;
+    public bool rotate;
+
+    void Start()
+    {
+        r = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Vector2 cameraPostion = new Vector2(Camera.main.transform.forward.x, Camera.main.transform.forward.z);
+        Vector2 parent = new Vector2(transform.forward.x, transform.forward.z);
+        float enemyAngle = Vector2.Angle(cameraPostion, parent);
+        Vector3 cross = Vector3.Cross(cameraPostion, parent);
+
+        if(cross.z > 0)
+        {
+            enemyAngle = 360 - enemyAngle;
+        }
+
+        if (enemyAngle > 135 && enemyAngle < 225)
+        {
+            r.sprite = front;
+        }
+        else if(enemyAngle > 45 && enemyAngle < 135)
+        {
+            r.sprite = left;
+        }
+        else if(enemyAngle > 225 && enemyAngle < 315)
+        {
+            r.sprite = right;
+        }
+        else if (enemyAngle > 315 || enemyAngle < 45)
+        {
+            r.sprite = back;
+        }
+    }
+}
