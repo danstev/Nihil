007e02bcdcb7c7f449242c97f84b3208d3974a2d
Dan Ward <dan.steve@hotmail.co.uk>
2018-06-10 17:52:26 +0100

    spawn then destroy popup

---
+++ Assets/Scripts/PopUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUp : MonoBehaviour {
+    public Animator anim;
+    private Text dmgText;
+
+	// Use this for initialization
+	void Start () {
+        AnimatorClipInfo[] clipinfo = anim.GetCurrentAnimatorClipInfo(0);
+        Destroy(this.gameObject, clipinfo[0].clip.length);
+
+        dmgText = anim.GetComponent<Text>();
+	}
+
+    public void SetText(string damage)
+    {
+        dmgText.text = damage;
+    }
+
+}
