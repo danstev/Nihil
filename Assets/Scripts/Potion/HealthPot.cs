bdb3e955c889ee26351aecb6c5fd7aa825b75628
Dan Ward <dan.steve@hotmail.co.uk>
2018-06-29 21:24:26 +0100

    imherit planin

---
+++ Assets/Scripts/Potion/HealthPot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPot : Item {
+
+    public void Interact(Stats s)
+    {
+        s.RecoverDamage(10);
+        Destroy(gameObject);
+        Debug.Log("Destroying self.");
+    }
+
+}
