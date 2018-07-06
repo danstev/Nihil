050c3df2468f2fd4bd9923ad856d0fb8f44f57b8
Dan Ward <dan.steve@hotmail.co.uk>
2018-07-06 15:35:05 +0100

    legs

---
+++ Assets/Scripts/Equip/Armours/Legs.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Legs : Armour {
+
+    public new void Interact(GameObject person)
+    {
+        //Not implemented
+    }
+
+}
