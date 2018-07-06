b311b0b59a257f229b268fba9ade51a5bf05836a
Dan Ward <dan.steve@hotmail.co.uk>
2018-07-06 15:34:44 +0100

    armour

---
+++ Assets/Scripts/Equip/Armour.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armour : Item {
+
+    public new void Interact(GameObject person)
+    {
+        //Not implemented
+    }
+}
