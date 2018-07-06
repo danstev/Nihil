7a038d79abd9ba26fb8d56f220805a7903f13738
Dan Ward <dan.steve@hotmail.co.uk>
2018-07-06 15:34:48 +0100

    amulet

---
+++ Assets/Scripts/Equip/Armours/Amulet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Amulet : Armour {
+
+    public new void Interact(GameObject person)
+    {
+        //Not implemented
+    }
+
+}
