fc22fc66e09abbceb1b5ce095e6575b7adc3e69d
Dan Ward <dan.steve@hotmail.co.uk>
2018-07-06 15:34:56 +0100

    body

---
+++ Assets/Scripts/Equip/Armours/Body.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Body : Armour {
+
+    public new void Interact(GameObject person)
+    {
+        //Not implemented
+    }
+
+}
