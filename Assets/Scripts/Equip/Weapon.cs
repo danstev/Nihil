385443b7eb96df4d8c121e99962c1abf9bbc5562
Dan Ward <dan.steve@hotmail.co.uk>
2018-07-09 17:54:59 +0100

    added coll deletion, inv placing, stats

--- Assets/Scripts/Equip/Weapon.cs
+++ Assets/Scripts/Equip/Weapon.cs
@@ -6,9 +6,23 @@ public class Weapon : Item {
 
     public new void Interact(GameObject person)
     {
+        //Eliminiate capsule collider
+        CapsuleCollider cap = GetComponent<CapsuleCollider>();
+        cap.isTrigger = false;
+        cap.radius = 0.0001f;
+        cap.height = .0001f;
+
+        //Place in inventory
         Inventory invent = person.GetComponent<Inventory>();
-        invent.slots[0] = this.gameObject;
+        invent.weapon = this.gameObject;
+
+        //Move to correct places
         transform.parent = person.transform;
-        transform.localPosition = new Vector3(0.43f, 0f, 0.53f);
+        transform.localPosition = new Vector3(0.43f, 0.25f, 0.53f);
+        transform.localRotation = Quaternion.Euler(20,90,-20);
+
+        //Update stats from newly equipped item.
+        Stats s = person.GetComponent<Stats>();
+        s.UpdateStats(health, power, defense, attack);
     }
 }
