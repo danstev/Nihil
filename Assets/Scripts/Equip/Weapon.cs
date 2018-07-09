905908b453caed39d682567cd07431c2f7ffb23c
Dan Ward <dan.steve@hotmail.co.uk>
2018-07-09 15:39:35 +0100

    haha local

--- Assets/Scripts/Equip/Weapon.cs
+++ Assets/Scripts/Equip/Weapon.cs
@@ -9,6 +9,6 @@ public class Weapon : Item {
         Inventory invent = person.GetComponent<Inventory>();
         invent.slots[0] = this.gameObject;
         transform.parent = person.transform;
-        transform.position = new Vector3(0.43f, 0f, 0.53f);
+        transform.localPosition = new Vector3(0.43f, 0f, 0.53f);
     }
 }
