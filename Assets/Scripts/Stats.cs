3c1b094499d25bfdc0740cf38e40401015704a05
Dan Ward <dan.steve@hotmail.co.uk>
2018-07-09 17:55:36 +0100

    health and stuff

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -12,19 +12,34 @@ public class Stats : NetworkBehaviour {
     //Knockback vars
     public Rigidbody r;
     public CharacterController player;
-    private Vector3 imp;
+    public Vector3 imp;
 
     [SyncVar(hook = "OnChangeHealth")]
     public int health = 0;
 
-    [SyncVar(hook = "OnChangePower")]
+    [SyncVar(hook = "OnChangePower")] //Even needed?
     public int power = 0;
 
     public int maxHealth = 100;
+    public int maxHealthBase = 100;
+    public int otherHealth = 0;
+
     public int maxPower = 50;
+    public int maxPowerBase = 50;
+    public int otherPower = 0;
+    public int powerGain = 1;
+
+    public int attack = 0;
+    public int otherattack = 0;
+
+    public int defense = 0;
+    public int otherDefense = 0;
+
+    public int expGranted = 10;
+
 
     //[SyncVar]
-    //private bool dead = false;
+    private bool dead = false;
 
     // Use this for initialization
     void Start () {
@@ -32,8 +47,7 @@ public class Stats : NetworkBehaviour {
 
         if (isLocalPlayer)
         {
-            healthText.text = "Health: " + health;
-            powerText.text = "Power: " + power;
+            UpdateStatsUI();
             //hover.enabled = false;
         }
         else
@@ -41,9 +55,43 @@ public class Stats : NetworkBehaviour {
             //hover.enabled = true;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    public void PowerLevel()
+    {
+        if(power >= maxPower)
+        {
+            power = maxPower;
+        }
+        else
+        {
+            power += powerGain;
+        }
+    }
+
+    public void UpdateStatsUI()
+    {
+        healthText.text = "Health: " + health + "/" + maxHealth;
+        powerText.text = "Power: " + power + "/" + maxPower;
+    }
+
+    public void UpdateStats(int health, int power, int addedDefense, int addedAttack)
+    {
+        maxHealth = maxHealthBase + health + otherHealth;
+        maxPower = maxPowerBase + power + otherPower;
+        attack = otherattack + addedAttack;
+        defense = otherDefense + addedDefense;
+        UpdateStatsUI();
+    }
+
+    public void UpdateStats()
+    {
+        maxHealth = maxHealthBase + otherHealth;
+        maxPower = maxPowerBase + otherPower;
+        UpdateStatsUI();
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         if(imp.magnitude > 0.2f)
         {
@@ -54,12 +102,19 @@ public class Stats : NetworkBehaviour {
             imp = Vector3.zero;
         }
         imp = Vector3.Lerp(imp, Vector3.zero, 5 * Time.deltaTime);
+
+        if(isLocalPlayer)
+        {
+            PowerLevel();
+        }
     }
 
     [Command]
     public void CmdTakeDamage(float[] incoming)
     {
-        health -= (int)incoming[0];
+        int dmg = ((int)incoming[0] - defense);
+        health -= dmg;
+        Debug.Log(gameObject.name + "has taken " + dmg + " damage.");
 
         Vector3 p = new Vector3(incoming[1], incoming[2], incoming[3]);
         p = transform.position - p;
@@ -107,11 +162,21 @@ public class Stats : NetworkBehaviour {
 
     public void Destroy()
     {
-        if(tag == "Enemy")
+        if (dead == false)
         {
-            GetComponent<EnemyAI>().SetDead();
-            StartCoroutine(Fade(GetComponentInChildren<Renderer>()));
-            Destroy(gameObject, 2f);
+            dead = true;
+            //Add exp to near players
+            GameObject game = GameObject.FindGameObjectWithTag("GameManager");
+            GameManager gm = game.GetComponent<GameManager>();
+            gm.AddExp(expGranted);
+
+            //Fade, not workin :p
+            if (tag == "Enemy")
+            {
+                GetComponent<EnemyAI>().SetDead();
+                StartCoroutine(Fade(GetComponentInChildren<Renderer>()));
+                Destroy(gameObject, 2f);
+            }
         }
     }
 
