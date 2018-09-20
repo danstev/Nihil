f2ec311e99e2877011b76bd371e9d487219ad2c9
Dan Ward <dan.steve@hotmail.co.uk>
2018-09-20 03:08:05 +0100

    Misc

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+    public string sceneChange;
+
+    public void Interact(GameObject g)
+    {
+        SceneManager.LoadScene(sceneChange, LoadSceneMode.Single);
+    }
+}
