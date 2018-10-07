d07f9c74af8d58f92d553849e31fbfaf004235b7
Dan Ward <dan.steve@hotmail.co.uk>
2018-10-07 23:13:32 +0100

    equip + unequip logic

--- Assets/Scripts/Equip/Weapon.cs
+++ Assets/Scripts/Equip/Weapon.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Weapon : Item {
 
+    public int health, power, attack, defense;
+    public Animator anim;
+
+    public void OnStart()
+    {
+        anim.PlayInFixedTime("spin", -1, 0);
+    }
+
     public new void Interact(GameObject person)
     {
         //Eliminiate capsule collider
@@ -18,11 +26,38 @@ public class Weapon : Item {
 
         //Move to correct places
         transform.parent = person.transform;
-        transform.localPosition = new Vector3(0.43f, 0.25f, 0.53f);
-        transform.localRotation = Quaternion.Euler(20,90,-20);
+        transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+        transform.localRotation = Quaternion.Euler(0,0,0);
 
         //Update stats from newly equipped item.
         Stats s = person.GetComponent<Stats>();
         s.UpdateStats(health, power, defense, attack);
+
+        //Animset
+        anim.PlayInFixedTime("idle", -1, 0);
+        //transform.localRotation = Quaternion.Euler(20, 90, -20);
+    }
+
+    public void Unequip(GameObject person)
+    {
+        //Remake capsule collider
+        CapsuleCollider cap = GetComponent<CapsuleCollider>();
+        cap.isTrigger = true;
+        cap.radius = 1f;
+        cap.height = 1f;
+
+        //Remove in inventory
+        Inventory invent = person.GetComponent<Inventory>();
+        invent.weapon = null;
+
+        //Move to correct places
+        transform.parent = null;
+
+        //Update stats from newly equipped item.
+        Stats s = person.GetComponent<Stats>();
+        s.UpdateStats(health, power, defense, attack);
+
+        //Animset
+        anim.PlayInFixedTime("spin", -1, 0);
     }
 }
