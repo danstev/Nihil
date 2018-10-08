eeb3c0bb56d2d998c3ced7fd15688b173c66cecb
Dan Ward <dan.steve@hotmail.co.uk>
2018-10-08 23:37:21 +0100

    transfer AUTH stuff for networking

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Item : MonoBehaviour {
+public class Item : NetworkBehaviour {
 
     public bool equipable;
     public int slotTaken;
@@ -14,7 +15,8 @@ public class Item : MonoBehaviour {
 
     }
 
-    public void Interact(GameObject go)
+    [Command]
+    public void CmdInteract(GameObject go)
     {
         Stats s = go.GetComponent<Stats>();
         s.RecoverDamage(10);
@@ -22,5 +24,42 @@ public class Item : MonoBehaviour {
         Debug.Log("Destroying self.");
     }
 
-    
+    public void TransferAuth(NetworkIdentity from, NetworkIdentity to)
+    {
+        if (from.clientAuthorityOwner != null)
+        {
+            from.RemoveClientAuthority(from.clientAuthorityOwner);
+        }
+
+        if (!isServer)
+        {
+            //from.AssignClientAuthority(to.connectionToServer);
+            //RpcSetParent(to);
+            //from.AssignClientAuthority(to.connectionToClient);
+        }
+        else
+        {
+            from.AssignClientAuthority(to.connectionToClient);
+            RpcSetParent(to);
+
+        }
+
+        //from.gameObject.transform.parent = to.gameObject.transform;
+    }
+
+    [ClientRpc]
+    public void RpcSetParent(NetworkIdentity to)
+    {
+        transform.SetParent(to.gameObject.transform);
+    }
+
+    public void RemoveAuth(NetworkIdentity from)
+    {
+        if (from.clientAuthorityOwner != null)
+        {
+            from.RemoveClientAuthority(from.clientAuthorityOwner);
+        }
+    }
+
+
 }
