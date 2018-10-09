14e85469b8bdc348ae93e8e6ea501a50b09bbb4c
Dan Ward <dan.steve@hotmail.co.uk>
2018-10-09 22:53:31 +0100

    small damager on coll script

---
+++ Assets/Scripts/Equip/WeaponCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCollision : MonoBehaviour {
+
+    public float attack;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name + " collided with " + gameObject.name + ".");
+        //Construct knockback params
+        float[] KnockbackAttack = new float[5];
+
+        //Damage
+        KnockbackAttack[0] = attack;
+        //Pos
+        KnockbackAttack[1] = transform.position.x;
+        KnockbackAttack[2] = transform.position.y;
+        KnockbackAttack[3] = transform.position.z;
+        //Angle
+        //KnockbackAttack[4] = transform.eulerAngles.y;
+
+        //Send
+        collision.transform.SendMessage(("CmdTakeDamage"), KnockbackAttack, SendMessageOptions.DontRequireReceiver);
+    }
+}
